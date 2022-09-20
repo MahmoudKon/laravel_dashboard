4c9962818485fb9a7d62c7797d9697764347bb24
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-09-20 15:28:31 +0200

    push

--- resources/views/messenger/includes/users.blade.php
+++ resources/views/messenger/includes/users.blade.php
@@ -34,9 +34,6 @@
                             </span>
                         </div>
 
-                        {{-- unread_count = 4 --}}
-                        {{-- user_unread_count = 0 --}}
-
                         <i class="fa-solid fa-check {{ $user_unread_count && !$unread_count ? '' : 'd-none' }} unread-message-icon"></i>
                         <i class="fa-solid fa-check-double text-success {{ (!$user_unread_count && !$unread_count) && $user->conversations->count() ? '' : 'd-none' }} read-message-icon"></i>
                         <div class="badge badge-circle bg-primary ms-5 unread-messages unread-messages-user-{{ $user->id }} {{ $unread_count ? '' : 'd-none' }}"> {{ $unread_count }} </div>
