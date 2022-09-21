43ea3251086bc99c6e636a7fde561ce4a44b9866
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-09-21 09:22:59 +0200

    push

--- app/Http/Middleware/UserLastSeen.php
+++ app/Http/Middleware/UserLastSeen.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -19,11 +18,8 @@ class UserLastSeen
     public function handle(Request $request, Closure $next)
     {
         if (auth()->check()) {
-            $expiresAt = now()->addMinutes(2); /* keep online for 2 min */
+            $expiresAt = now()->addMinutes(5); /* keep online for 5 min */
             Cache::put('user-is-online-' . auth()->id(), true, $expiresAt);
-
-            /* last seen */
-            User::where('id', auth()->id())->update(['last_seen' => now()]);
         }
 
         return $next($request);
