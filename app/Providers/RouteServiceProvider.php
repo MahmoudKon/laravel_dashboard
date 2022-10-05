962ecfa6b4c964fef13f023ad444b6045579eb6e
mahmoud mohammed <mahmoud.mohammed@ivas.com.eg>
2022-10-05 09:04:00 +0200

    add middleware for backend routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -49,7 +49,7 @@ class RouteServiceProvider extends ServiceProvider
             ->namespace("$this->namespace")
                 ->group(base_path('routes/web.php'));
 
-            Route::middleware(['web', 'auth'])
+            Route::middleware(['web', 'auth', 'localeSessionRedirect', 'localizationRedirect'])
                 ->prefix(LaravelLocalization::setLocale()."/".URL_PREFIX)
                 ->namespace("$this->namespace\Backend")
                 ->as(ROUTE_PREFIX)
